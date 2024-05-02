f54d7aaed72328a69b69316d50cee502bb7e8251
Arthur <arthurldh@gmail.com>
2024-05-02 18:38:04 +0200

    teardown

--- src/credit_score_package/credit_score_event.py
+++ src/credit_score_package/credit_score_event.py
@@ -1,16 +1,16 @@
 class CreditScoreEvent:
     def __init__(self):
-        self.lines = []
+        self.failed_lines = []
 
     def add_line(self, status, status_message, line, index):
-        self.lines.append({
-            'index': index,
-            'status_code': status,
-            'status_message': status_message,
-            'line': line
-        })
+        if status == 0:
+            self.failed_lines.append({
+                'index': index,
+                'status_code': status,
+                'status_message': status_message,
+                'line': line
+            })
 
     def get_failed_lines(self):
-        for line in self.lines:
-            if line['status_code'] == 0:
-                print(line)
+        for line in self.failed_lines:
+            print(line)
