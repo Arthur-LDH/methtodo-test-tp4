f84cc2937e49690ea4bc3f326be7bc2138ba447d
Arthur <arthurldh@gmail.com>
2024-05-02 15:30:38 +0200

    add first test

--- src/credit_score_package/test/credit_score_interface_test.py
+++ src/credit_score_package/test/credit_score_interface_test.py
@@ -8,3 +8,11 @@ class CreditScoreInterfaceTest(unittest.TestCase):
         with self.assertRaises(Exception):
             CreditScoreInterface('empty.csv').exec()
 
+    def test_exec_with_csv_file_with_wrong_headers(self):
+        with self.assertRaises(Exception):
+            CreditScoreInterface('wrong_headers.csv').exec()
+
+    def test_exec_with_csv_file_with_no_data(self):
+        with self.assertRaises(Exception):
+            CreditScoreInterface('no_data.csv').exec()
+
