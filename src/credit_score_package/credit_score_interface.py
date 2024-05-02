dd35b736c5147a22e8682895aa58814eee165222
Arthur <arthurldh@gmail.com>
2024-05-02 13:04:38 +0200

    initial commit

---
+++ src/credit_score_package/credit_score_interface.py
@@ -0,0 +1,9 @@
+class CreditScoreInterface :
+    def __init__(self, credit_score_service):
+        self.credit_score_service = credit_score_service
+
+    def exec(self, args):
+        return;
+
+    def exec_line(self, line):
+        return;
